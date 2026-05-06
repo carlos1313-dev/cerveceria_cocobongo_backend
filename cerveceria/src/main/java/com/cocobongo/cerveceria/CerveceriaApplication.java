df5e15fd8cc948a7a69ed97732c0a57946f5d864
Carlos E <sangroniscarlosenrique1@gmail.com>
2026-05-05 21:34:38 -0500

    Add: @EnableAsync en CerveceriaApplication para permitir la anotación de async en diversos métodos

--- cerveceria/src/main/java/com/cocobongo/cerveceria/CerveceriaApplication.java
+++ cerveceria/src/main/java/com/cocobongo/cerveceria/CerveceriaApplication.java
@@ -2,8 +2,10 @@ package com.cocobongo.cerveceria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync   // Habilita el @Async en AuditService
 public class CerveceriaApplication {
 
 	public static void main(String[] args) {
